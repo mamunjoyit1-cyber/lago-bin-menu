a45d03d27c51a76bdd90b31912ba8ecd6617aabe
mamunjoyit1-cyber <mamunjoyit1@gmail.com>
2026-09-17 15:37:40 +0200

    Update preload.js

--- electron-app/preload.js
+++ electron-app/preload.js
@@ -5,6 +5,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
   silentPrint: (html, printerName) => ipcRenderer.invoke('silent-print', { html, printerName }),
   silentPrintIP: (base64Data, ip, port) => ipcRenderer.invoke('silent-print-ip', { base64Data, ip, port }),
   listPrinters: () => ipcRenderer.invoke('list-printers'),
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
   onUpdateAvailable: (callback) => ipcRenderer.on('update-available', (event, data) => callback(data)),
   openDownloadPage: () => ipcRenderer.invoke('open-download-page')
 });
